764bc1ca0822bf64a5b4544afee96594f1cb9dcf
Sam Francis <sfrancis@mindgrub.com>
2018-12-26 21:31:57 -0500

    Initial Commit

---
+++ CoverArt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoverArt
+//
+//  Created by Sam Francis on 12/26/18.
+//  Copyright © 2018 SamFrancis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CoverArt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoverArt
+//
+//  Created by Sam Francis on 12/26/18.
+//  Copyright © 2018 SamFrancis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
